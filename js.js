2bc606e028acbfb312d3130ecfab015f701eb46b
yasra <mahdibekaran123@gmail.com>
2024-10-10 22:33:21 +0200

    ddsaa

--- js.js
+++ js.js
@@ -2,3 +2,7 @@ let todovalue = 12 ;
 length.toExponential;
 window.addEventListener();
 webkitURL.apply();
+let todovalue = 12 ;
+length.toExponential;
+window.addEventListener();
+webkitURL.apply();
